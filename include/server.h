f99c03fea84875959d0bea77017144adc0e42224
jkubuszek <jkubuszek@student.agh.edu.pl>
2026-01-13 14:59:05 +0100

    added signal handlers to server, changed users.txt to .csv and moved it, added main to server

--- include/server.h
+++ include/server.h
@@ -5,6 +5,27 @@
  * @date 2026-01-13
  */
 
+
+
+typedef struct{
+    char word[32];
+    char mask[32]
+}GameSession;
+
+/// @brief sigchild handler
+/// @param signo signal number, passed by linux kernel
+void sig_chld(int signo);
+
+/// @brief sigpipe handler
+/// @param signo signal number, passed by linux kernel
+void sig_pipe(int signo);
+
+/// @brief function used for checking the guess
+/// @param session GameSession struct that defines current session's word and word mask
+/// @param letter a letter to process
+/// @return returns 1 if comepleted succesfully, -1 if encountered an error
+int process_guess(GameSession session, char letter);
+
 /// @brief function used for authentication
 /// @param username 32 byte char array used for storing the username
 /// @param password 32 byte char array used for storing the password
@@ -13,4 +34,4 @@ int authenticate_player(const char username, char password);
 
 /// @brief function used for taking care of 1 player
 /// @param player_socket socket number the player is connected to
-void handle_player(int player_socket);
+void handle_client(int client_socket);
--- src/server.c
+++ src/server.c
@@ -11,16 +11,34 @@
 #include        <stdio.h>
 #include        <stdlib.h>
 #include        <string.h>
-#include 	<unistd.h>
+#include 	    <unistd.h>
+#include        <signal.h>
+#include        <wait.h>
 #include    "protocol.h"
 #include    "server.h"
 
+#define LISTENQ 10
+#define MAXLINE 1024
+
+void sig_chld(int signo){
+	pid_t	pid;
+	int		stat;
+	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+		printf("child %d terminated\n", pid);
+	return;
+}
+
+void sig_pipe(int signo){
+	printf("Server received SIGPIPE - Default action is exit \n");
+	exit(1);
+}
+
 int authenticate_player(const char username, const char password){
 
-    FILE *file = fopen("users.txt", "r");
+    FILE *file = fopen("users.csv", "r");
     
     if (file == NULL) {
-        perror("ERROR: couldn't open/find users.txt");
+        perror("ERROR: couldn't open/find users.csv");
         return 0; 
     }
 
@@ -28,7 +46,7 @@ int authenticate_player(const char username, const char password){
     char file_pass[MAX_LEN];
     int auth_success = 0; 
 
-    while (fscanf(file, "%31s %31s", file_user, file_pass) == 2) {
+    while (fscanf(file, "%31s;%31s", file_user, file_pass) == 2) {
         if (strcmp(username, file_user) == 0 && strcmp(password, file_pass) == 0) {
             auth_success = 1;
             break; 
@@ -38,9 +56,73 @@ int authenticate_player(const char username, const char password){
     fclose(file);
     return auth_success;
 }
-   
+
+int process_guess(GameSession session, char letter){
+
+}
+
+void handle_client(int connfd) {
+}
+
+int main(int argc, char **argv)
+{
+	int				listenfd, connfd;
+    char				buff[MAXLINE], str[INET_ADDRSTRLEN+1];
+	socklen_t			len;
+	struct sockaddr_in	servaddr, cliaddr;
 
 
-int main(){
+        if ( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+                fprintf(stderr,"socket error : %s\n", strerror(errno));
+                return 1;
+        }
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr   = htonl(INADDR_ANY);
+	servaddr.sin_port   = htons(8080);	
+
+        if ( bind( listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+                fprintf(stderr,"bind error : %s\n", strerror(errno));
+                return 1;
+        }
+
+        if ( listen(listenfd, LISTENQ) < 0){
+                fprintf(stderr,"listen error : %s\n", strerror(errno));
+                return 1;
+        }
+
+    signal(SIGCHLD, sig_chld);
+	signal(SIGPIPE, sig_pipe);
+
+	fprintf(stderr,"Waiting for clients ... \n");
+	for ( ; ; ) {
+		len = sizeof(cliaddr);
+        	if ( (connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) < 0){
+                	fprintf(stderr,"accept error : %s\n", strerror(errno));
+                	continue;
+        	}
+
+		bzero(str, sizeof(str));
+	   	inet_ntop(AF_INET, (struct sockaddr  *) &cliaddr.sin_addr,  str, sizeof(str));
+		printf("Connection from %s\n", str);
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("ERROR: fork error");
+            close(connfd);
+        } 
+        else if (pid == 0) {
+            close(listenfd);
+            handle_client(connfd);
+            close(connfd);
+            exit(0); 
+        } 
+        else {
+            close(connfd);
+        }
+    }
+
     return 0;
 }
